ad4f14a96b94a7e201c960646e1093b0edd88df0
marufmarzuq <marufmarzuq18@gmail.com>
2023-11-04 19:11:18 +0600

    Grid style completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { Box } from "@mui/material";
+import Gallery from "gallery";
+
 const App = () => {
-  return <div>Welcome</div>;
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        bgcolor: "#efefef",
+      }}
+    >
+      <Gallery />
+    </Box>
+  );
 };
 
 export default App;
